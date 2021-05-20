c7d0247f8ac38168db5722b8708c4ce4dd1d8d00
JohanFrom <gouldsilver@gmail.com>
2021-05-20 15:50:08 +0200

    Skriva till localstorage och skrivs ut på bevakningar

--- projektfpa/src/LocalStorage.js
+++ projektfpa/src/LocalStorage.js
@@ -1,45 +1,69 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function LocalStorage(props) {
 
-    function loadWatchList() {
-        let getStorage = localStorage.getItem("cryptoData")
-        let storedData = JSON.parse(getStorage);
+    // Lista för datan som ska skrivas till localStorage
+    const [local, setLocal] = useState([])
 
-        if (storedData === null) {
-            // Om det inte finns någon lista att hämta, skapa en tom
+    function loadWatchList() {
+        // Hämtar datan med nyckeln "cryptoData" i localStorage
+        let getStorage = localStorage.getItem("cryptoData")
+
+        // Kontrollerar om getStorage är null
+        if (getStorage === null) {
+            // Om det inte finns någon lista att hämta, skapa en tom lista och gör om till en sträng
             localStorage.setItem("cryptoData", JSON.stringify([]));
             return [];
         } else {
-            console.log("STORED DATA");
-            console.log(storedData);
-            return storedData
+            // Gör om datan från localStorage till ett objekt
+            let parsedStorage = JSON.parse(getStorage)
+            return parsedStorage
         }
     }
-        
-    function addToWatchlist() {
-
-        const loadData = loadWatchList();
-        loadData.push(props.cryptoData)
-
-        localStorage.setItem("cryptoData", JSON.stringify(loadData));
-
-    }
-
     
-    const test = loadWatchList();
+    function addToWatchList() {
+        // Lägger till data om valutan i listan 
+        local.push({
+            name: props.cryptoData[0]["key"],
+            price: props.cryptoData[2]["key"],
+            date: props.cryptoData[1]["key"]
+        });
+        
+        // Skriver till localStorage
+        localStorage.setItem("cryptoData", JSON.stringify(local));
 
-    for (let i = 0; i < test.length; i++) {
-        console.log(test[i][i]);
+        // Uppdaterar listan som håller i data om valutan
+        setLocal([...local]);
+    }   
+
+        // Tar bort filmen som man klickar på (krysset)
+    function removeFromWatchList(event) {
+        let test = this.target
+        console.log(test);
+        let storage = localStorage.getItem("cryptoData");
+        // localStorage.removeItem(storage)
+        // setLocal(local.filter((currency) => currency.name !== name));
+
     }
-  
+    
+    
+        
+    
+    
+    // Kallar på funktionen som skriver ut vad som finns i localStorage
+    const printLocalStorage = loadWatchList();
+
     return (
         <div>
-            <button className="btn btn-success" onClick={addToWatchlist}>Lägg till bevakning</button>
-            <h4>Mina horövervakningar</h4>
-            <table>
-                { test.map((hej, index) =>
-                    <tr className="list-unstyled text-center">{hej[index].name} - { hej[index].key }</tr>
+            <button className="btn btn-success" onClick={addToWatchList}>Lägg till bevakning</button>
+            <h4>Mina övervakningar</h4>
+            <table>                
+            {printLocalStorage.map((item) => 
+                    <tr>{item.name} {item.price} {item.date}
+                        <button type="button" class="close" aria-label="Close" onClick={removeFromWatchList}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </tr>
                 )}
             </table>
         </div>
